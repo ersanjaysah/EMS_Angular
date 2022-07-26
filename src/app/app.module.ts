d31d22d6898a6eedf094acc9029e076c92a4abd4
Sanjay Sah <Sanjays@emids.com>
2022-07-26 08:12:20 -0700

    [sanjay]Added:More UI Design done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SigninComponent } from './component/signin/signin.component';
-import { HomeComponent } from './component/home/home.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-import { EmpLoginComponent } from './component/emp-login/emp-login.component';
+import {EmpLoginComponent } from './component/emp-login/emp-login.component';
+import {FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+
+import { SigninComponent } from './component/signin/signin.component';
+import { HomeComponent } from './component/home/home.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { UpdateComponent } from './component/update/update.component';
+import { DialogComponent } from './component/dialog/dialog.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SigninComponent,
     HomeComponent,
-    EmpLoginComponent
+    EmpLoginComponent,
+    DashboardComponent,
+    UpdateComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,MatInputModule,MatCheckboxModule
+    MatFormFieldModule,MatInputModule,MatCheckboxModule,MatDialogModule,
+    ReactiveFormsModule,MatToolbarModule,MatIconModule,MatButtonModule,
+    MatSelectModule,FormsModule,HttpClientModule,MatTableModule,MatPaginatorModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
