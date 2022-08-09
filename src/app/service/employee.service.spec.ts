4f7a2d0c3c7a99c9b6180af642370d47c88018d8
Sanjay Sah <Sanjays@emids.com>
2022-08-09 09:16:39 -0700

    [sanjay]Added: Unit testing

--- src/app/service/employee.service.spec.ts
+++ src/app/service/employee.service.spec.ts
@@ -1,16 +1,28 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { EmployeeService } from './employee.service';
 
-describe('EmployeeService', () => {
-  let service: EmployeeService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(EmployeeService);
+describe('EmployeeService', () => {
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [EmployeeService]
+  }));
+
+  it(' component created', () => {
+    const http: EmployeeService = TestBed.get(EmployeeService);
+    expect(http).toBeTruthy();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('Testing of EmployeeLogin function', () => {
+    const http: EmployeeService = TestBed.get(EmployeeService);
+    expect(http.employeeSignin).toBeTruthy();
+  });
+
+  it('Testing of EmployeeDetails Function', () => {
+    const http: EmployeeService = TestBed.get(EmployeeService);
+    expect(http.getemployee).toBeTruthy();
   });
 });
--- src/app/service/http.service.spec.ts
+++ src/app/service/http.service.spec.ts
@@ -1,16 +1,23 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { HttpService } from './http.service';
 
-describe('HttpService', () => {
-  let service: HttpService;
+describe(' MY HttpService', () => {
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HttpService);
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [HttpService]
+  }));
+
+  it('service created', () => {
+    const http: HttpService = TestBed.get(HttpService);
+    expect(http).toBeTruthy();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('service test getData function', () => {
+    const http: HttpService = TestBed.get(HttpService);
+    expect(http.getService).toBeTruthy();
   });
+
 });
