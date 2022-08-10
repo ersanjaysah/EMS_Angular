8233b0b8912bdfa2fcb708309436549cfaa2eedb
Sanjay Sah <Sanjays@emids.com>
2022-08-10 09:29:00 -0700

    [sanjay]Added: Unit testing of component

--- src/app/component/signin/signin.component.spec.ts
+++ src/app/component/signin/signin.component.spec.ts
@@ -1,25 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormGroup, ReactiveFormsModule, FormsModule, FormBuilder } from '@angular/forms';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
 import { SigninComponent } from './signin.component';
 
-describe('SigninComponent', () => {
+
+describe('LoginComponent', () => {
   let component: SigninComponent;
   let fixture: ComponentFixture<SigninComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SigninComponent ]
-    })
-    .compileComponents();
+    beforeEach(async(() => {
+     TestBed.configureTestingModule({
+       declarations: [SigninComponent],
+      imports: [
+         
+         ReactiveFormsModule,
+         RouterTestingModule,
+         HttpClientModule,
+         MatSnackBarModule
+      ],
+    }).compileComponents();
+   }));
+   
+   beforeEach(() => {
+     fixture =   TestBed.createComponent(SigninComponent);
+     component = fixture.componentInstance;
+     fixture.detectChanges();
+   });
+
+  it('should create a form using formbuilder', () => {
+     expect(component).toBeTruthy();
+   });
+
+   it('should contain component', () => {
+    expect(component.loginForm).toBeTruthy();
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SigninComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should contain login form of default value', () => {
+    expect(component.loginForm.value).toEqual({email:'', password:''});
   });
 });
